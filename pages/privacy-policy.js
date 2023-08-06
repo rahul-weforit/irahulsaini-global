9858e2ba5aa456ed199b54da509e8c93e666d65b
Rahul Saini <rahulsaini@Rahuls-MacBook-Air.local>
2023-08-06 16:19:52 +0530

    update

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -55,7 +55,7 @@ export default function Home(){
 								<p><a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
 							</li>
 						</ul> 
-						<h4>Privacy Policies</h4>
+						<h4>Privacy Policies</h4> 
 						<p>You may consult this list to find the Privacy Policy for each of the advertising partners of hello@irahulsaini.com.</p>
 						<p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on hello@irahulsaini.com, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 						<p>Note that hello@irahulsaini.com has no access to or control over these cookies that are used by third-party advertisers.</p>
